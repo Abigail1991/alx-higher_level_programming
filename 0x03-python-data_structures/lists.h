c1b74a4170b0448824f24cd8ea6e342d272901ae
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-09 09:05:31 -0800

    data structures

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include < unistd.h>
+
+def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
+def max_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
+def delete_at(my_list=[], idx=0):
+
+#endif
