3d7325a9fe99b21501106708944b4257bd699c5f
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-13 07:35:13 -0800

    more data structure

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    a = 0
+
+    if type(roman_string) is str and roman string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if rom[roman_string[c]] >= a:
+                res += rom[roman-string[c]]
+            else:
+                res -= rom[roman_string[c]]
+            a = rom[roman_string[c]]
+            return res
