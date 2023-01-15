bbe69a218487f2375b2b7b457707e0d94cec76e7
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-14 22:39:57 -0800

    fix errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     a = 0
 
     if type(roman_string) is str and roman string:
         for c in range(len(roman_string) - 1, -1, -1):
-            if rom[roman_string[c]] >= a:
-                res += rom[roman-string[c]]
+            if val[roman_string[c]] >= a:
+                res += val[roman-string[c]]
             else:
-                res -= rom[roman_string[c]]
-            a = rom[roman_string[c]]
+                res -= val[roman_string[c]]
+            a = val[roman_string[c]]
             return res
