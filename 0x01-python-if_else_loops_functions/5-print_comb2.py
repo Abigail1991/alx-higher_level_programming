4844d95a5a374b3e7870dc729d35a278aa22086e
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-15 01:29:57 -0800

    fix errors

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    print("{}".format(number))
-else:
-    print("{:02}",format(number), end=", ")
+for number in range(0, 99 + 1):
+    if (number == 99):
+        print("{:2d}".format(number))
+    else:
+        print("{:02d},".format(number), end='')
