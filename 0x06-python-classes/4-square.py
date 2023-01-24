d243b99d043d355d3dea73e9414d8c39875e6ecf
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-24 13:48:17 -0800

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square and its size and checking
+if the given values are right, and a setter and getter methods to set or get
+it. There's also an area method that returns the area of the square.
+"""
+
 
 class Square():
+    """Defines a square."""
+
     def __init__(self, size=0):
+        """Sets the necessary attributes for the Square object.
+         Args:
+            size (int): the size of one edge of the square.
+        """
         self.__size = size
 
     @property
     def size(self):
+        """Get or set the size of the square."""
         return self.__size
 
     @size.setter
@@ -17,4 +30,6 @@ class Square():
         self.__size = value
 
     def area(self):
+        """Returns the current square area."""
+
         return self.__size ** 2
