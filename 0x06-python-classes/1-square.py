8b878b1efe40b068604e9df23c7a5675405618a5
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-24 13:34:22 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" Square Module"""
+
 
 class Square():
+    """Class representing a square"""
+
     def __init__(self, size):
+        """ Sets the necessary attributes for the Square object
+        Args:
+            size (int): the size of one edge of the square.
+        """
         self.__size = size
