61232b6c640745af8368be7feb366404994b3625
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-24 13:51:17 -0800

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
+
 
 class Square():
+    """square class with it's size and proper validation"""
 
     def __init__(self, size=0):
         self.__size = size
