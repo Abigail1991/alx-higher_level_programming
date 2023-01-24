16e0db6616dbac4783dad61032b61dc6d7c6a2e2
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2023-01-24 13:42:36 -0800

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
+
 
 class Square():
+    """square class with it's size and proper validation"""
+
     def __init__(self, size=0):
         if (type(size) is not int):
             raise TypeError("size must be an integer")
